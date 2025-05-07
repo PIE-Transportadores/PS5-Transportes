05e4c3ac5ebbc51537df405191386657c3c890bf
João Vitor <joaovitortba@gmail.com>
2025-05-07 09:55:28 -0300

    Commit inicial

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Tela_gerenciamento from "./tela_gerenciamento/page";
+
+export default function Home() {
+  return (
+
+    <div>
+      <Tela_gerenciamento/>
+
+    </div>
+    
+    
+  );
+}
---
+++ src/app/tela_gerenciamento/page.tsx
@@ -0,0 +1,13 @@
+
+
+export default  function Tela_gerenciamento(){
+
+    return(
+        <div>
+
+            <h1>Ola mundo</h1>      
+        </div>
+    )
+
+
+}
