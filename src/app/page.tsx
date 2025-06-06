a9d96a71934ed1e908183270e76f3983a9dbe540
Deyuarute <arthur13nd@gmail.com>
2025-06-06 15:48:43 -0400

    home v1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-
+/*
 'use client'
 import Tela_gerenciamento from "./tela_gerenciamento/page";
 import Viws_func from "./tela_gerenciamento/Gerenciamento-funcionario/page";
@@ -11,5 +11,23 @@ export default function Home() {
     </div>
     
     
+  );
+}
+*/
+'use client'; 
+
+export default function Home() {
+  return (
+    <div>
+      <iframe
+        src="/index.html" // Caminho para o arquivo na pasta 'public'
+        style={{
+          width: '100%',     // Ocupa toda a largura disponível
+          height: '100vh',   // Ocupa toda a altura da viewport (tela)
+          border: 'none'     // Remove a borda padrão do iframe
+        }}
+        title="Página Inicial da Transportadora" // Título acessível para o iframe
+      />
+    </div>
   );
 }
